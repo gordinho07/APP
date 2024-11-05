5b054fede1c46ebd975f47d19543adef7bc4fe33
Luiz <hugostoso25nutri@gmail.com>
2024-11-05 14:29:10 -0300

    Aula dia 5/11

--- cadastro.js
+++ cadastro.js
@@ -1,123 +1,317 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import {
+  StyleSheet, Text, TextInput, View,
+  TouchableOpacity, Alert, Image,
+  ActivityIndicator, ScrollView
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Cadastro = () => {
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [acceptTerms, setAcceptTerms] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [showMoreTerms, setShowMoreTerms] = useState(false);
+
+  // Função para verificar a força da senha
+  const isPasswordStrong = (password) => {
+    const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return strongPasswordRegex.test(password);
+  };
+
+  // Função para determinar o nível da força da senha
+  const getPasswordStrength = (password) => {
+    if (password.length < 6) {
+      return { strength: 'Fraca', color: 'red', progress: 25 };
+    }
+    if (isPasswordStrong(password)) {
+      return { strength: 'Forte', color: 'green', progress: 100 };
+    }
+    return { strength: 'Média', color: 'orange', progress: 50 };
+  };
+
+  const handleRegister = () => {
+    if (!email.includes('@')) {
+      setErrorMessage('Por favor, insira um email válido.');
+      return;
+    }
+    if (password.length < 6) {
+      setErrorMessage('A senha precisa ter pelo menos 6 caracteres.');
+      return;
+    }
+    if (!isPasswordStrong(password)) {
+      setErrorMessage('A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial.');
+      return;
+    }
+    if (password !== confirmPassword) {
+      setErrorMessage('As senhas não coincidem.');
+      return;
+    }
+
+    setLoading(true);
+    setErrorMessage('');
+
+    setTimeout(() => {
+      setLoading(false);
+      Alert.alert('Sucesso', `Cadastro realizado para: ${email}`);
+      setEmail('');
+      setPassword('');
+      setConfirmPassword('');
+      setAcceptTerms(false);
+      setShowMoreTerms(false);
+    }, 2000);
+  };
+
+  const { strength, color, progress } = getPasswordStrength(password);
+
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Image source={require('./assets/logo.png')} style={styles.logo} />
-        <Text style={styles.title}>Cadastro</Text>
-      </View>
-      <View style={styles.form}>
-        <Text style={styles.label}>Nome</Text>
-        <View style={styles.inputForm}>
-          <MaterialIcons name="person" size={24} color="#333" />
-          <TextInput style={styles.input} placeholder="Digite seu nome" />
-        </View>
-        <Text style={styles.label}>E-mail</Text>
-        <View style={styles.inputForm}>
-          <MaterialIcons name="email" size={24} color="#333" />
-          <TextInput style={styles.input} placeholder="Digite seu e-mail" />
-        </View>
-        <Text style={styles.label}>Senha</Text>
-        <View style={styles.inputForm}>
-          <MaterialIcons name="lock" size={24} color="#333" />
-          <TextInput style={styles.input} placeholder="Digite sua senha" secureTextEntry />
-        </View>
-        <Text style={styles.label}>Confirmar Senha</Text>
-        <View style={styles.inputForm}>
-          <MaterialIcons name="lock" size={24} color="#333" />
-          <TextInput style={styles.input} placeholder="Confirme sua senha" secureTextEntry />
-        </View>
-        <TouchableOpacity style={styles.buttonSubmit}>
-          <Text style={{ color: 'white', fontSize: 16, fontWeight: '500' }}>Cadastrar</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity>
-          <Text style={styles.span}> Já tem uma conta? Entrar</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Image
+        source={require('../APP/assets/logo.png')} // Substitua pelo caminho da sua imagem
+        style={styles.logo}
+      />
+      <Text style={styles.title}>Crie sua Conta</Text>
+      {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null}
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#A9A9A9"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        onFocus={() => setErrorMessage('')}
+      />
+
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          placeholderTextColor="#A9A9A9"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={!showPassword}
+        />
+        <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={styles.iconContainer}>
+          <Icon name={showPassword ? "eye-slash" : "eye"} size={20} color="#8C52FF" />
         </TouchableOpacity>
       </View>
-    </View>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Confirmar Senha"
+        placeholderTextColor="#A9A9A9"
+        value={confirmPassword}
+        onChangeText={setConfirmPassword}
+        secureTextEntry={!showConfirmPassword}
+        onFocus={() => setErrorMessage('')}
+      />
+      <TouchableOpacity onPress={() => setShowConfirmPassword(!showConfirmPassword)} style={styles.iconContainer}>
+        <Icon name={showConfirmPassword ? "eye-slash" : "eye"} size={20} color="#8C52FF" />
+      </TouchableOpacity>
+
+      {/* Barra de progresso de força da senha */}
+      {password.length > 0 && (
+        <View style={styles.passwordStrengthContainer}>
+          <Text style={[styles.passwordStrengthText, { color: color }]}>{strength}</Text>
+          <View style={[styles.passwordStrengthBar, { backgroundColor: color, width: `${progress}%` }]} />
+        </View>
+      )}
+
+      {/* Checkbox para Aceitar os Termos */}
+      <View style={styles.termsContainer}>
+        <TouchableOpacity onPress={() => setAcceptTerms(!acceptTerms)}>
+          <View style={[styles.checkbox, acceptTerms && styles.checked]}>
+            {acceptTerms && <Text style={styles.checkboxTick}>✔</Text>}
+          </View>
+        </TouchableOpacity>
+        <Text style={styles.termsText}>Aceito os termos de serviço</Text>
+      </View>
+
+      {/* Detalhes dos Termos de Uso */}
+      {acceptTerms && (
+        <View style={styles.termsDetailsContainer}>
+          <Text style={styles.termsDetails}>
+            <Text style={styles.bold}>Termos e Condições de Uso do Aplicativo "Anjos Da Guarda"</Text>
+            {"\n"}Última atualização: 05 de Novembro de 2024
+            {"\n\n"}Bem-vindo ao "Anjos Da Guarda"! Ao acessar ou utilizar nosso aplicativo, você concorda em cumprir e se submeter aos seguintes Termos e Condições de Uso...
+          </Text>
+          <TouchableOpacity onPress={() => setShowMoreTerms(!showMoreTerms)}>
+            <Text style={styles.moreTermsText}>
+              {showMoreTerms ? 'Menos detalhes' : 'Leia mais...'}
+            </Text>
+          </TouchableOpacity>
+
+          {/* Exibe os detalhes adicionais quando o usuário clicar "Leia mais..." */}
+          {showMoreTerms && (
+            <View style={styles.moreDetailsContainer}>
+            <Text style={styles.moreDetails}>
+              <Text style={styles.bold}>1. Definições: </Text>
+              "Anjos Da Guarda": O aplicativo oferecido por alunos da Etec Cidade Tiradentes, que tem como objetivo ser um aplicativo de TCC.
+              {"\n"}Usuário: Qualquer pessoa que faça uso do aplicativo, seja como visitante ou usuário registrado.
+
+              {"\n\n"}<Text style={styles.bold}>2. Aceitação dos Termos: </Text>
+              Ao acessar ou usar o "Anjos Da Guarda", você reconhece que leu, entendeu e concorda em cumprir estes Termos. Se você não concorda com qualquer parte destes Termos, não use o App.
+
+              {"\n\n"}<Text style={styles.bold}>3. Cadastro e Conta de Usuário: </Text>
+              Para utilizar determinadas funcionalidades do "Anjos Da Guarda", pode ser necessário criar uma conta. Ao criar sua conta, você se compromete a fornecer informações precisas, completas e atualizadas, e manter sua conta segura e confidencial. Você é o único responsável pela utilização de sua conta, inclusive se ela for acessada por terceiros sem sua permissão.
+
+              {"\n\n"}<Text style={styles.bold}>4. Uso do App: </Text>
+              Você concorda em usar o "Anjos Da Guarda" de forma responsável e em conformidade com as leis e regulamentos aplicáveis. Você não poderá:
+              - Usar o App para fins ilícitos ou de forma que prejudique a experiência de outros usuários.
+              - Modificar, descompilar ou fazer engenharia reversa do App ou de seu código-fonte.
+              - Compartilhar informações ou conteúdos prejudiciais, fraudulentos, abusivos ou que violem direitos de terceiros.
+
+              {"\n\n"}<Text style={styles.bold}>5. Propriedade Intelectual: </Text>
+              Todo o conteúdo e design do "Anjos Da Guarda", incluindo, mas não se limitando a, textos, imagens, logotipos, ícones e software, são de propriedade exclusiva do Anjos Da Guarda ou licenciados por terceiros.
+
+              {"\n\n"}<Text style={styles.bold}>6. Privacidade e Coleta de Dados: </Text>
+              A proteção da sua privacidade é importante para nós. Ao usar o "Anjos Da Guarda", você concorda com a coleta, uso e compartilhamento de seus dados pessoais conforme descrito em nossa Política de Privacidade.
+            </Text>
+          </View>
+          )}
+        </View>
+      )}
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleRegister}
+        disabled={loading}
+      >
+        {loading ? (
+          <ActivityIndicator color="#fff" />
+        ) : (
+          <Text style={styles.buttonText}>Cadastrar</Text>
+        )}
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => Alert.alert('Redirecionar para login')}>
+        <Text style={styles.registerText}>Já tem uma conta? Faça Login</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-  header: {
-    alignItems: 'center',
-    marginBottom: 20,
+    padding: 20,
+    backgroundColor: '#F7F9FC',
   },
   logo: {
-    width: 150,
-    height: 150,
-    resizeMode: 'contain',
+    width: 120,
+    height: 120,
+    alignSelf: 'center',
+    marginBottom: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 30,
+    marginBottom: 20,
+    textAlign: 'center',
     fontWeight: 'bold',
     color: '#8C52FF',
   },
-  form: {
-    width: '80%',
-    padding: 30,
-    backgroundColor: '#ffffff',
-    borderRadius: 20,
-    shadowColor: '#8C52FF',
-    shadowOpacity: 0.2,
-    shadowRadius: 10,
-    elevation: 5,
-    
-  },
-  inputForm: {
-    
-    borderColor: '#8C52FF',
-    borderWidth: 1.5,
-    borderRadius: 10,
-    height: 50,
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingLeft: 10,
-    marginBottom: 20,
+  error: {
+    color: '#FF6B6B',
+    marginBottom: 12,
+    textAlign: 'center',
   },
   input: {
-    marginLeft: 10,
-    borderRadius: 10,
-    borderColor: 'transparent',
-    width: '100%',
-    height: '100%',
-    fontSize: 16,
-    color: '#8C52FF',
-  },
-  buttonSubmit: {
-    marginTop: 20,
-    backgroundColor: '#8C52FF',
-    borderColor: 'transparent',
-    borderRadius: 10,
     height: 50,
-    width: '100%',
-    justifyContent: 'center',
+    borderColor: '#8C52FF',
+    borderWidth: 2,
+    borderRadius: 25,
+    marginBottom: 12,
+    paddingHorizontal: 16,
+    backgroundColor: '#fff',
+  },
+  passwordContainer: {
+    position: 'relative',
+  },
+  iconContainer: {
+    position: 'absolute',
+    right: 15,
+    top: 15,
+  },
+  passwordStrengthContainer: {
+    marginBottom: 10,
     alignItems: 'center',
   },
-  label: {
-    color: '#8C52FF',
-    fontWeight: '600',
-    fontSize: 16,
+  passwordStrengthText: {
+    fontSize: 14,
+    fontWeight: 'bold',
     marginBottom: 5,
   },
-  span: {
-    marginTop: 25,
+  passwordStrengthBar: {
+    height: 5,
+    borderRadius: 5,
+    width: '100%',
+    backgroundColor: 'gray',
+  },
+  termsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    borderColor: '#8C52FF',
+    borderWidth: 2,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 8,
+  },
+  checked: {
+    backgroundColor: '#8C52FF',
+  },
+  checkboxTick: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  termsText: {
+    fontSize: 16,
+    color: '#333',
+  },
+  termsDetailsContainer: {
+    marginTop: 10,
+  },
+  termsDetails: {
     fontSize: 14,
-    marginLeft: 5,
+    color: '#333',
+  },
+  moreTermsText: {
     color: '#8C52FF',
-    fontWeight: '500',
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: '#8C52FF',
+    borderRadius: 25,
+    padding: 15,
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  registerText: {
+    color: '#8C52FF',
+    textAlign: 'center',
+    marginTop: 12,
+    fontWeight: '600',
+  },
+  bold: {
+    fontWeight: 'bold',
   },
 });
 
-export default Cadastro;
-//
+export default Register;
--- components/CardNoticia.js
+++ components/CardNoticia.js
@@ -1,177 +1,160 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { View, FlatList, Text, TouchableOpacity, Image, StyleSheet, Linking } from 'react-native';
 
-// Dados dos cartões para diferentes categorias
-const noticias = [
-  {
-    image: require('../assets/banner.jpeg'),
-    title: 'Notícia 1',
-    description: 'Descrição da notícia 1',
-  },
-  {
-    image: require('../assets/banner.jpeg'),
-    title: 'Notícia 2',
-    description: 'Descrição da notícia 2',
-  },
-  {
-    image: require('../assets/banner.jpeg'),
-    title: 'Notícia 3',
-    description: 'Descrição da notícia 3',
-  },
-];
-
-const ongs = [
-  {
-    image: require('../assets/banner.jpeg'),
-    title: 'ONG 1',
-    description: 'Descrição da ONG 1',
-  },
-  {
-    image: require('../assets/banner.jpeg'),
-    title: 'ONG 2',
-    description: 'Descrição da ONG 2',
-  },
-  {
-    image: require('../assets/banner.jpeg'),
-    title: 'ONG 3',
-    description: 'Descrição da ONG 3',
-  },
-];
-
-const psicologos = [
-  {
-    image: require('../assets/banner.jpeg'),
-    title: 'Psicólogo 1',
-    description: 'Descrição do psicólogo 1',
-  },
-];
-
-// Componente Card
-const Card = ({ image, title, description, onPress }) => {
-  return (
-    <View style={styles.card}>
-      <Image source={image} style={styles.image} />
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.description}>{description}</Text>
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text style={styles.buttonText}>Saiba mais</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-// Componente CardNoticia
 const CardNoticia = () => {
-  const [currentIndex, setCurrentIndex] = useState(0); // Índice do item atual
-  const [showPsychologist, setShowPsychologist] = useState(false); // Estado para controlar exibição do psicólogo
+  // Array de objetos contendo a imagem, título, descrição e link para cada "cardNoticia"
+  const cardNoticias = [
+    {
+      id: '1',
+      image: require('../assets/psico1.png'),  // Usando require() para imagens locais
+      title: 'Felipe Fernandes',
+      description: 'Psicanalista Clínico',
+      link: 'mailto:felipeff3179@outlook.com', // Link para abrir o e-mail diretamente
+    },
+    {
+      id: '2',
+      image: require('../assets/ong1.png'),  // Usando uma URL externa
+      title: 'Aldeias Infantis SOS',
+      description: 'Descrição da Notícia 2',
+      link: 'https://doe.aldeiasinfantis.org.br/torne-se-um-amigo-sos/people/new?utm_source=site&utm_medium=menu&utm_campaign=doacao', // Link para página da ONG
+    },
+    {
+      id: '3',
+      image: require('../assets/psico2.png'),
+      title: 'Rubens dos Santos',
+      description: 'Psicologia Social',
+      link: 'mailto:felipeff3179@outlook.com', // Link para abrir o e-mail diretamente
+    },
+    {
+      id: '4',
+      image: { uri: 'https://via.placeholder.com/150' },
+      title: 'Notícia 4',
+      description: 'Descrição da Notícia 4',
+      link: 'https://www.exemplo.com/noticia4',
+    },
+    {
+      id: '5',
+      image: { uri: 'https://via.placeholder.com/150' },
+      title: 'Notícia 5',
+      description: 'Descrição da Notícia 5',
+      link: 'mailto:felipeff3179@outlook.com', // Link para abrir o e-mail diretamente
+    },
+    {
+      id: '6',
+      image: { uri: 'https://via.placeholder.com/150' },
+      title: 'Notícia 6',
+      description: 'Descrição da Notícia 6',
+      link: 'https://www.exemplo.com/noticia6',
+    },
+    {
+      id: '7',
+      image: { uri: 'https://via.placeholder.com/150' },
+      title: 'Notícia 7',
+      description: 'Descrição da Notícia 7',
+      link: 'https://www.exemplo.com/noticia7',
+    },
+  ];
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Função para avançar automaticamente os cards
   useEffect(() => {
     const intervalId = setInterval(() => {
-      if (currentIndex < noticias.length) {
-        setCurrentIndex((prevIndex) => prevIndex + 1);
+      if (currentIndex + 2 < cardNoticias.length) {
+        setCurrentIndex(currentIndex + 2);
       } else {
-        setShowPsychologist(true);
-        setCurrentIndex(-1); // Prepara para exibir o psicólogo
+        setCurrentIndex(0); // Volta para o início ao chegar ao final
       }
-    }, 5000);
+    }, 3000); // Atualiza a cada 3 segundos
 
     return () => clearInterval(intervalId); // Limpa o intervalo ao desmontar o componente
   }, [currentIndex]);
 
-  useEffect(() => {
-    if (showPsychologist) {
-      const timeoutId = setTimeout(() => {
-        setShowPsychologist(false);
-        setCurrentIndex(0); // Reinicia o índice para a primeira notícia
-      }, 5000); // Exibe o psicólogo por 5 segundos
+  // Função para abrir o link do botão
+  const handlePress = (link) => {
+    Linking.openURL(link); // Abre o link da página ou e-mail correspondente
+  };
 
-      return () => clearTimeout(timeoutId); // Limpa o timeout ao desmontar
-    }
-  }, [showPsychologist]);
+  const renderCardNoticia = ({ item }) => (
+    <View style={styles.card}>
+      <Image source={item.image} style={styles.cardImage} />
+      <Text style={styles.cardTitle}>{item.title}</Text>
+      <Text style={styles.cardDescription}>{item.description}</Text>
+      <TouchableOpacity 
+        style={styles.button} 
+        onPress={() => handlePress(item.link)} // Passa o link do card
+      >
+        <Text style={styles.buttonText}>Saiba Mais</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  // Slice o array de cardNoticias para mostrar dois itens de cada vez
+  const visibleCards = cardNoticias.slice(currentIndex, currentIndex + 2);
 
   return (
     <View style={styles.container}>
-      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} contentContainerStyle={styles.scrollContainer}>
-        {showPsychologist ? (
-          <Card
-            {...psicologos[0]} // Apenas um psicólogo
-            onPress={() => console.log(`Botão pressionado para ${psicologos[0].title}`)}
-          />
-        ) : (
-          <>
-            {currentIndex >= 0 && currentIndex < noticias.length && (
-              <>
-                <Card
-                  {...noticias[currentIndex]}
-                  onPress={() => console.log(`Botão pressionado para ${noticias[currentIndex].title}`)}
-                />
-                <Card
-                  {...ongs[currentIndex]}
-                  onPress={() => console.log(`Botão pressionado para ${ongs[currentIndex].title}`)}
-                />
-              </>
-            )}
-          </>
-        )}
-      </ScrollView>
+      <FlatList
+        data={visibleCards}
+        renderItem={renderCardNoticia}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 };
 
-// Estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-  scrollContainer: {
-    paddingHorizontal: 16,
-    alignItems: 'center',
+    backgroundColor: '#f4f4f4',
+    paddingHorizontal: 20,
   },
   card: {
-    flexDirection: 'column',
+    backgroundColor: '#8C52FF',
+    borderRadius: 12,
+    margin: 10,
+    padding: 20,
+    width: 180,
     alignItems: 'center',
-    padding: 8,
-    margin: 4,
-    backgroundColor: '#ffffff',
-    borderRadius: 10,
-    shadowColor: '#8C52FF',
+    justifyContent: 'center',
+    elevation: 4, // Shadow effect for Android
+    shadowColor: '#000', // Shadow for iOS
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
+    shadowOpacity: 0.8,
     shadowRadius: 4,
-    width: 100,
-    height: 220,
   },
-  image: {
-    width: 80,
-    height: 80,
-    borderRadius: 10,
+  cardImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60, // Tornando a imagem arredondada
+    marginBottom: 10,
   },
-  title: {
-    fontSize: 16,
+  cardTitle: {
+    color: '#fff',
+    fontSize: 15,
+    textAlign: 'center',
     fontWeight: 'bold',
-    marginVertical: 4,
+    marginBottom: 5,
   },
-  description: {
-    fontSize: 12,
-    color: '#666',
+  cardDescription: {
+    color: '#fff',
+    fontSize: 14,
+    textAlign: 'center',
+    marginBottom: 10,
   },
   button: {
-    backgroundColor: '#8C52FF',
-    borderColor: 'transparent',
-    borderRadius: 10,
-    height: 30,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 10,
+    backgroundColor: '#2ecc71', // Cor do botão
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
   },
   buttonText: {
-    color: 'white',
-    fontSize: 14,
-    fontWeight: '500',
+    color: '#fff',
+    fontSize: 16,
   },
 });
 
